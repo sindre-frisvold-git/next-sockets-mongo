827baf48e1e2f476a093a93b5bf8c3ce7a85bd10
Sindre <sinfr91@gmail.com>
2022-05-29 15:07:59 +1200

    add socket connection

--- pages/api/sockets.js
+++ pages/api/sockets.js
@@ -1,3 +1,11 @@
+import { Server } from 'socket.io'
+
 export default function socket(req, res) {
-  console.log(req.socket)
+  if (res.socket.server.io) console.log('server running')
+  else {
+    console.log('initialising')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+  }
+  res.end()
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react'
+import { io } from 'socket.io-client'
+let socket
+
 export default function Home() {
+  useEffect(() => socketInitialiser(), [])
+  async function socketInitialiser() {
+    await fetch('/api/sockets')
+    socket = io()
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+  }
   return (
     <div>
       <p>Hello World</p>
